6b031077709b8bf0c93b7adda0b41660000729ea
yacareadmin <ovd@gmx.net>
2016-08-06 21:12:26 +0200

    added the less then 10 test

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+
 var Clock = require('Clock');
 
 describe('Clock', () => {
@@ -16,6 +17,15 @@ describe('Clock', () => {
       var clock = TestUtils.renderIntoDocument(<Clock />);
       var seconds = 615;
       var expected = '10:15';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should format seconds when min/sec are less than 10', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock />);
+      var seconds = 61;
+      var expected = '01:01';
       var actual = clock.formatSeconds(seconds);
 
       expect(actual).toBe(expected);
